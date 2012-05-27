35a25d337946e51deb14da1b4111372a4ae9eb11
Sean Smith <strmshdw@gmail.com>
2012-05-27 16:52:17 -0700

    Attempting to render contacts with ember.

--- emberDemo/emberDemo/Scripts/views/contacts.js
+++ emberDemo/emberDemo/Scripts/views/contacts.js
@@ -1,10 +1,16 @@
 ﻿require.config({
-    baseUrl: '.',
+    baseUrl: '',
     paths: {
-        'jquery':'scripts/libraries/jquery-1.6.2.min'
+        'jquery': 'scripts/libraries/jquery-1.6.2.min',
+        'ember' : 'scripts/libraries/ember-0.9.8.1.min',
+        'text' : 'scripts/libraries/text'
     }
 });
 
-require(['jquery', 'scripts/services/contactService'], function ($, cs) {
-    cs.getAll(function (contacts) { debugger });
+require(['jquery', 'ember', 'scripts/services/contactService', 'text!scripts/templates/contacts.htm'], function ($, _ember, cs, template) {
+    var App = Ember.Application.create({ rootElement: '#app' });
+    App.Contact = Ember.Object.extend({ FirstName: null, LastName: null, Email: null, Priority: -1, Phone: null });
+    App.contactsView = Ember.View.create({template: Ember.Handlebars.compile(template), contacts: [] });
+
+    cs.getAll(function (contacts) { App.contactsView.contacts = contacts; debugger});
 });
