339e776f82bd29097896996e39c9235891da0810
Sean Smith <strmshdw@gmail.com>
2012-05-27 15:08:26 -0700

    Added require and ember libraries.  Setup require and service for contacts.

---
+++ emberDemo/emberDemo/Scripts/services/contactService.js
@@ -0,0 +1,9 @@
+﻿define(['jquery'], function ($) {
+    var cs = function () { };
+
+    cs.prototype.getAll = function (callback) {
+        $.ajax({ type: 'GET', url: '/api/contacts', success: callback });
+    }
+
+    return new cs;
+});
---
+++ emberDemo/emberDemo/Scripts/views/contacts.js
@@ -0,0 +1,10 @@
+﻿require.config({
+    baseUrl: '.',
+    paths: {
+        'jquery':'scripts/libraries/jquery-1.6.2.min'
+    }
+});
+
+require(['jquery', 'scripts/services/contactService'], function ($, cs) {
+    cs.getAll(function (contacts) { debugger });
+});
