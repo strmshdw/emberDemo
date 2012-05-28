3ac045712f11889a901b3a6276124a19eaf4d098
Sean Smith <strmshdw@gmail.com>
2012-05-28 16:52:40 -0700

    Added window.App

--- emberDemo/emberDemo/Scripts/views/contacts.js
+++ emberDemo/emberDemo/Scripts/views/contacts.js
@@ -8,9 +8,9 @@
 });
 
 require(['jquery', 'ember', 'scripts/services/contactService', 'text!scripts/templates/contacts.htm'], function ($, _ember, cs, template) {
-    var App = Ember.Application.create({ rootElement: '#app' });
+    window.App = Ember.Application.create({ rootElement: '#app' });
     App.Contact = Ember.Object.extend({ FirstName: null, LastName: null, Email: null, Priority: -1, Phone: null });
-    App.contactsView = Ember.View.create({template: Ember.Handlebars.compile(template), contacts: [] });
+    App.contactsView = Ember.View.create({ template: Ember.Handlebars.compile(template), contacts: [] });
 
-    cs.getAll(function (contacts) { App.contactsView.contacts = contacts; debugger});
+    cs.getAll(function (contacts) { App.contactsView.contacts = contacts; debugger });
 });
