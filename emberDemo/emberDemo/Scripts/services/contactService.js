e03811106fd0298698e028bdf4986152ab31426a
Sean Smith <strmshdw@gmail.com>
2012-05-31 11:14:06 -0700

    Added removal.

--- emberDemo/emberDemo/Scripts/services/contactService.js
+++ emberDemo/emberDemo/Scripts/services/contactService.js
@@ -1,12 +1,24 @@
 ﻿define(['jquery'], function ($) {
     var cs = function () { };
-    
+
     cs.prototype.getAll = function (callback) {
         $.ajax({ type: 'GET', url: '/api/contacts', success: callback });
     }
 
+    cs.prototype.get = function (id, callback) {
+        $.ajax({ type: 'GET', url: '/api/contacts/' + id, success: callback });
+    }
+
     cs.prototype.add = function (contact, callback) {
         $.ajax({ type: 'PUT', url: '/api/contacts', contentType: "application/json;charset=utf-8", data: JSON.stringify(contact), success: callback });
+    }
+
+    cs.prototype.update = function (contact, callback) {
+        $.ajax({ type: 'POST', url: '/api/contacts/', contentType: "application/json;charset=utf-8", data: JSON.stringify(contact), success: callback });
+    }
+
+    cs.prototype.remove = function (id, callback) {
+        $.ajax({ type: 'DELETE', url: '/api/contacts/' + id, success: callback });
     }
 
     return new cs;
--- emberDemo/emberDemo/Scripts/views/contacts.js
+++ emberDemo/emberDemo/Scripts/views/contacts.js
@@ -14,7 +14,17 @@ require(['jquery', 'ember', 'scripts/services/contactService', 'text!scripts/tem
         }
     });
 
-    App.Contact = Ember.Object.extend({ FirstName: null, LastName: null, Email: null, Priority: -1, Phone: null });
+    App.Contact = Ember.Object.extend({
+        FirstName: null,
+        LastName: null,
+        Email: null,
+        Priority: -1,
+        Phone: null,
+        isSelected: false,
+        toJSON: function () {
+            return this.getProperties('FirstName', 'LastName', 'Email', 'Priority', 'Phone');
+        }
+    });
 
     App.contactsController = Ember.ArrayController.create({
         content: [],
@@ -25,23 +35,72 @@ require(['jquery', 'ember', 'scripts/services/contactService', 'text!scripts/tem
         },
         add: function (contact) {
             var _this = this;
-            cs.add(contact, function () { _this.getAll(); });
+            cs.add(contact.toJSON(), function (id) {
+                contact.set('Id', id);
+                _this.pushObject(contact);
+            });
+        },
+        get: function (id) {
+            cs.get(id);
+        },
+        remove: function (id) {
+            var _this = this;
+            cs.remove(id, function () {
+                var toRemove, i;
+                var content = _this.content;
+
+                for (i = 0; i < content.length; i++) {
+                    if (content[i].get('Id') == id) {
+                        toRemove = content[i];
+                    }
+                }
+
+                if (toRemove) {
+                    _this.removeObject(toRemove);
+                }
+
+            });
+        },
+        removeSelected: function () {
+            var i;
+            for (i = 0; i < this.content.length; i++) {
+                if (this.content[i].get('isSelected')) {
+                    this.remove(this.content[i].get('Id'));
+                }
+            }
+        },
+        update: function (contact) {
+            cs.update(contact);
         },
         updateContent: function (content) {
             var i;
             this.clear();
             for (i = 0; i < content.length; i++) {
-
                 this.pushObject(App.Contact.create(content[i]));
             }
         }
     });
 
     App.ContactsView = Ember.View.extend({ template: Ember.Handlebars.compile(template) });
+
     App.AddContactView = Ember.View.extend({
         tagName: 'a',
         attributeBindings: ['href'],
-        click: function () { App.contactsController.add({ FirstName: 'Sean' }); }
+        click: function () { App.contactsController.add(App.Contact.create({ FirstName: 'Sean' })); }
     });
 
+    App.DeleteContactView = Ember.View.extend({
+        tagName: 'span',
+        eventManager: {
+            click: function (event, view) {
+                App.contactsController.remove(view.content.Id);
+            }
+        }
+    });
+
+    App.RemoveContactsView = Ember.View.extend({
+        click: function () {
+            App.contactsController.removeSelected();
+        }
+    });
 });
